5212371c5dacd467e3a3466d2e8724c7cc45e89f
Mukesh Singh <mukesh@Mukesh-Office-Mac.local>
2024-08-16 11:22:25 +0530

    Update first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ['*'],
   theme: {
+    screens: {
+      xs: '375',
+      sm: '575px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1600px',
+    },
+
     extend: {},
   },
   plugins: [],
